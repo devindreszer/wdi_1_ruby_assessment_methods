720a0ca56a80f62bfe3c6b9e4c7c72c7d01d73b0
Devin Dreszer <devindreszer@gmail.com>
2014-06-12 10:34:32 -0400

    Complete Methods Assessment

--- methods.rb
+++ methods.rb
@@ -6,7 +6,7 @@ end
 
 y = square(2)
 # What is the value of 'x' after this line executes?
-
+# nil; undefined local variable
 
 #### Question 2
 # What is the value of `age` after we call `get_older`?
@@ -17,6 +17,8 @@ end
 
 age = 10
 get_older(age)
+
+# age = 10; the result of get_older(age) is 11, but it is not assigned to a variable
 
 
 #### Question 3
@@ -34,11 +36,17 @@ end
 energy = 2
 jump
 
+# you will get an error; undefined local variable 'energy' within how_high method
+
 
 #### Question 4
 # Write a method that accepts a string and an optional multiplier (defaults to 3)
 # and returns the original string multiplied that many times (using the * operator).
 # Do not use an options hash or keyword arguments.
+
+def multiplied_string(string, multiplier=3)
+  string * multiplier
+end
 
 
 #### Question 5
@@ -48,3 +56,5 @@ def greeting(name:, salutation: 'Hi', volume: :low)
   greeting = "#{salutation} there, #{name}"
   volume == :high ? greeting.upcase : greeting
 end
+
+greeting(name: "puny humans", salutation: 'hello', volume: :high)
